c387104ddf327de6cf6132a5a9b7b4709fd8af83
Kacper <kusiakkacper@gmail.com>
2020-12-16 12:50:53 +0100

    api keys

--- App.js
+++ App.js
@@ -8,19 +8,11 @@ import operationsReducer from "./store/reducers/operations";
 import usersReducer from "./store/reducers/user";
 import expendituresReducer from "./store/reducers/expenditures";
 import { combineReducers, createStore, applyMiddleware } from "redux";
+import { fbConfig } from "./config/FirebaseConfig";
 import thunk from "redux-thunk";
 import firebase from "firebase/app";
 import "firebase/auth";
 export default function App() {
-  const fbConfig = {
-    apiKey: "AIzaSyDgnjNh1Ih3dcecgEbnM8yKdIb_CAMGat0",
-    authDomain: "settleup-85b43.firebaseapp.com",
-    databaseURL: "https://settleup-85b43.firebaseio.com",
-    projectId: "settleup-85b43",
-    storageBucket: "settleup-85b43.appspot.com",
-    messagingSenderId: "409124269870",
-    appId: "1:409124269870:web:c161699a0d362515c794bf",
-  };
   if (!firebase.apps.length) {
     firebase.initializeApp(fbConfig);
     firebase.firestore();
